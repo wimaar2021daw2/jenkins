d6d6c652b049cf9f873986bba5b7412c331533bc
dc1415daw2 <dc1415daw2@gamil>
2017-02-15 14:07:26 +0100

    Prova d'IC amb Jenkins

---
+++ src/jenkins/Jenkins.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jenkins;
+/**
+ *
+ * @author dc1617daw2
+ */
+import java.io.*;
+public class Jenkins {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        File f = new File("index.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");bw.newLine();
+            bw.write("  <head>");bw.newLine();
+            bw.write("    <title>");bw.newLine();
+            bw.write("      Treballant amb l'IC Jenkins");bw.newLine();
+            bw.write("    </title>");bw.newLine();
+            bw.write("  </head>");bw.newLine();
+            bw.write("  <body>");bw.newLine();
+            bw.write("    Projecte per provar l'IC Jenkins");bw.newLine();
+            bw.write("  </body>");bw.newLine();
+            bw.write("</html>");bw.newLine();
+            bw.close();
+        }
+    }
+}
